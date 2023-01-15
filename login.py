f9c49f134d269ad63212cfe753f2ff8674ee0a7e
Toby <tspitcher2010@gmail.com>
2023-01-15 18:04:41 +0000

    Add Sign Up Function

--- login.py
+++ login.py
@@ -1,16 +1,36 @@
-credentials = {
-	"testUser": "testPass",
-	"gwo0d": "password123",
-    "toby": "tobyisthebest"
-}
+import json
 
-UserReq = input("What is your username: ")
-PassReq = input("What is your password: ")
 
-try:
-	if credentials[UserReq] == PassReq:
-		print("Logged in!")
-	else:
+credentials = {}
+
+with open("creds.json", "r") as f:
+	credentials = json.loads(f.read())
+
+menu = int(input("Enter 1 to login and 2 to sign up: "))
+
+if menu == 1:
+
+	UserReq = input("What is your username: ")
+	PassReq = input("What is your password: ")
+
+	try:
+		if credentials[UserReq] == PassReq:
+			print("Logged in!")
+		else:
+			print("Login failed!")
+	except KeyError:
 		print("Login failed!")
-except KeyError:
-	print("Login failed!")
+
+elif menu == 2:
+	NewUser = input("Please select a username: ")
+	NewPass = input("Please select your password: ")
+
+	try:
+		credentials[NewUser]
+		print("Username unavailable! Please select another.")
+	except KeyError:
+		credentials[NewUser] = NewPass
+		print("Sign up succesful.")
+
+with open("creds.json", "w") as f:
+		f.write(json.dumps(credentials))
